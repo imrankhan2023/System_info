537ef472fe64a0166f59729f23ede99b3e142ebb
imrankhan2023 <90782350+imrankhan2023@users.noreply.github.com>
2021-10-02 12:59:22 +0530

    Add files via upload

---
+++ system_info.py
@@ -0,0 +1,10 @@
+import platform
+# no need to pip install
+print("System :", platform.system())
+print("Node :", platform.node())
+print("Platform :", platform.platform())
+print("Processor :", platform.processor())
+print('Python build no. and date:', platform.python_build())
+print('Python version:', platform.python_version())
+print('Machine type:', platform.machine())
+print('Platform architecture:', platform.architecture())
